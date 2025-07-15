24ef03def3e155e209ad71ba332d5a86d8a412c6
Joao Vitor <jvitorss2021@gmail.com>
2025-07-15 09:48:55 -0300

    Feat: Adicionar modelo e factory para o curso, incluindo migração da tabela

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'duration' => $this->faker->numberBetween(1, 52), // Duration in weeks
+            'price' => $this->faker->randomFloat(2, 0, 1000), // Price between 0 and 1000
+        ];
+    }
+}
